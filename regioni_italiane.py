b20e32aafa30e124bb72b0c44fa59305a8993a1e
Daniele Mancinelli <mancinelli.daniele@iisgalvanimi.edu.it>
2024-04-22 08:58:44 +0000

    aggiunto interazioni con le regioni

--- regioni_italiane.py
+++ regioni_italiane.py
@@ -10,6 +10,20 @@ def homepage():
     regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
     risultato = sorted(list(set(regioni_italiane['denominazione_regione'])))
     return render_template('regioni_italiane.html', lista = risultato)
+@app.route('/info/<regione>', methods=['GET'])
+def info(regione):
+  import pandas as pd
+  regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
+  info = regioni_italiane[regioni_italiane.denominazione_regione == regione]
+  return render_template('table.html', tabella = info.to_html())
+
+@app.route('/info_json/<regione>', methods=['GET'])
+def info_json(regione):
+  import pandas as pd
+  regioni_italiane = pd.read_csv('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-statistici-riferimento/popolazione-istat-regione-range.csv')
+  info = regioni_italiane[regioni_italiane.denominazione_regione == regione]
+  return render_template('json.html', tabella = info.to_json())
+
 
 
 if __name__ == '__main__':
