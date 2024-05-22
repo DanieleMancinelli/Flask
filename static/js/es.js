1b884b035462e31d96570dd8eefb1adb5f0365cc
Daniele Mancinelli <mancinelli.daniele@iisgalvanimi.edu.it>
2024-05-22 17:45:53 +0000

    Correggimento

--- static/js/es.js
+++ static/js/es.js
@@ -1,11 +1,11 @@
 function getElenco() {
-    fetch('https://3246-daniele0777-flask-w05ladh5qti.ws-eu110.gitpod.io/elenco')
+    fetch('https://3246-danielemancinell-flask-wo0l08dojlo.ws-eu114.gitpod.io/elenco')
     .then(response => response.json())
     .then(data => {
         document.getElementById('elenco').innerHTML = ''
         console.log(data)
         for (let regione in data) {
-            document.getElementById('elenco').innerHTML += '<a href=https://3246-daniele0777-flask-w05ladh5qti.ws-eu110.gitpod.io/info/' + data[regione] + '>' + data[regione] + '</a>' + '<br />'
+            document.getElementById('elenco').innerHTML += '<a href=https://3246-danielemancinell-flask-wo0l08dojlo.ws-eu114.gitpod.io/info/' + data[regione] + '>' + data[regione] + '</a>' + '<br />'
         }
     })
 }
